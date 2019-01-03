f5b0ba468b4116beb89d6fa85d2a4428538d64c0
utmostof9 <utmostof9@gmail.com>
2019-01-03 12:41:01 +0800

    gogo

--- models_taipei/convGRU/ubuntu_PC_run.bash
+++ models_taipei/convGRU/ubuntu_PC_run.bash
@@ -1,8 +1,8 @@
 # I size = 60, F size = 60
 python convGRU_run.py \
-    --root-dir /home/jack/storage_ssd/OneDrive/01_IIS/04_TY_research/01_Radar_data/02_numpy_files \
-    --ty-list-file /home/jack/storage_ssd/OneDrive/01_IIS/04_TY_research/ty_list.xlsx \
-    --result-dir /home/jack/storage_ssd/OneDrive/01_IIS/04_TY_research/05_results \
-    --I-lat-l 24.55 --I-lat-h 25.4375 --I-lon-l 121.15 --I-lon-h 122.0375 \
-    --F-lat-l 24.55 --F-lat-h 25.4375 --F-lon-l 121.15 --F-lon-h 122.0375 \
+    --root-dir /home/jack/OneDrive/01_IIS/04_TY_research/01_Radar_data/02_numpy_files \
+    --ty-list-file /home/jack/OneDrive/01_IIS/04_TY_research/ty_list.xlsx \
+    --result-dir /home/jack/OneDrive/01_IIS/04_TY_research/05_results \
+    --I-lat-l 24.6625 --I-lat-h 25.4 --I-lon-l 121.15 --I-lon-h 121.8875 \
+    --F-lat-l 24.6625 --F-lat-h 25.4 --F-lon-l 121.15 --F-lon-h 121.8875 \
     --weight-decay 0.1 --gpu 0
