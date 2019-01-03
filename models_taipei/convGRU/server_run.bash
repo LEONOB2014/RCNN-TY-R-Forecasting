7206493bf076344dccef8e6f5abd920c4e10be25
UTMOSTOF9 <utmostof9@gmail.com>
2019-01-03 12:26:54 +0800

    asa

--- models_taipei/convGRU/server_run.bash
+++ models_taipei/convGRU/server_run.bash
@@ -3,6 +3,4 @@ python convGRU_run.py \
     --root-dir /home/utmostof9/OneDrive/01_IIS/04_TY_research/01_Radar_data/02_numpy_files \
     --ty-list-file /home/utmostof9/OneDrive/01_IIS/04_TY_research/ty_list.xlsx \
     --result-dir /home/utmostof9/OneDrive/01_IIS/04_TY_research/05_results \
-    --I-lat-l 24.6625 --I-lat-h 25.4 --I-lon-l 121.15 --I-lon-h 121.8875 \
-    --F-lat-l 24.6625 --F-lat-h 25.4 --F-lon-l 121.15 --F-lon-h 121.8875 \
     --weight-decay 0.1  --gpu 1
