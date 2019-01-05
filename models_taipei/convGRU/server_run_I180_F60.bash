7f2a3d10ae9385ef0831aed0c26148c55b2b1cbc
utmostof9 <utmostof9@gmail.com>
2019-01-05 17:18:15 +0800

    Adjust bash files

---
+++ models_taipei/convGRU/server_run_I180_F60.bash
@@ -0,0 +1,6 @@
+# I size = 180, F size = 60
+python convGRU_run.py \
+    --root-dir /home/utmostof9/GoogleDrive/01_IIS/04_TY_research/01_Radar_data/02_numpy_files \
+    --ty-list-file /home/utmostof9/GoogleDrive/01_IIS/04_TY_research/ty_list.xlsx \
+    --result-dir /home/utmostof9/GoogleDrive/01_IIS/04_TY_research/04_results \
+    --weight-decay 0.1  --gpu 1
